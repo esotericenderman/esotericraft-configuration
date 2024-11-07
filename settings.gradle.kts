5c66d25ffc8c8ba93d2be298c9786c8e2cb2188c
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-07 13:06:08 +0000

    Configure Gradle plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
+pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            setUrl("https://maven.fabricmc.net/")
+        }
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "minecraft-configuration"
 
 include("mod-menu")
